0afbb1080cb39b7b5982c43e1af73dafc4a3fe63
Moon Light「つかり」 <MoonLMC1212@gmail.com>
2023-02-25 04:25:25 +0000

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/*.html',"./node_modules/flowbite/**/*.js"],
+  content: ['./src/*.html',"./node_modules/flowbite/**/*.js","./node_modules/aos/dist/aos.js"],
   theme: {},
   variants: {},
   plugins: [require("daisyui")],
