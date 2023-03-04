8cbc8057bde02f95a97dfb6ff7a188c63c29f762
Moon Light「つかり」 <MoonLMC1212@gmail.com>
2023-03-04 17:12:26 +0000

    takut dimas apus semua

--- src/assets/js/pengurus.js
+++ src/assets/js/pengurus.js
@@ -6,28 +6,54 @@ async function main() {
 	const data = text
 		.split("\n")
 		.filter((a) => a.length > 1)
-    .filter((a) => !a.includes("//"))
-    .filter((a) => !a.includes("--"));
-    let html = ""
+		.filter((a) => !a.includes("//"))
+		.filter((a) => !a.includes("--"));
+	let html = "";
 
-  let showingPengurus = text.split("\n").filter((a) => a.includes("INTI"))
-  const inti = showingPengurus[0].split("-")[5].trim()
-  for (let i = 0; i < data.length; i++) {
+  console.log(data)
+  let angkatanList = []
+	for (let i = 0; i < data.length; i++) {
 		const element = data[i];
 		const [nama, tipe, angkatan] = element.split(" - ");
-			html += `
-      <div class="card lg:w-96 w-80 bg-base-100 shadow-xl m-auto">
-          <div class="card-body items-center text-center">
-              <h2 class="card-title">${nama}</h2>
-              <p>${tipe} ${angkatan}</p>
-          </div>
-  </div>`;
-	}
-  document.querySelector("#data-struktur").innerHTML += html;
-  const swiper = new Swiper("#data-struktur", {
-    horizontal: true,
-  });
 
+    if(!angkatanList.includes(angkatan.replace(" ",""))){
+      angkatanList.push(angkatan.replace(" ",""))
+    }
+	}
+    // resort to be from higher to lower
+    angkatanList.sort((a, b) => b - a);
+	for (let i = 0; i < angkatanList.length; i++) {
+    const element = angkatanList[i];
+    let curHtml = ""
+    let showing = data.filter((a) => a.includes(element))
+	  for (let j = 0; j < showing.length; j++) {
+      const element = showing[j];
+      const [nama, tipe, angkatan] = element.split(" - ");
+      curHtml += `
+        <div class="card lg:w-96 w-80 bg-base-100 shadow-xl m-auto">
+           <div class="card-body items-center text-center">
+                <h2 class="card-title">${nama}</h2>
+                <p>${tipe} ${angkatan}</p>
+           </div>
+        </div>
+    `;
+    }
+    document.querySelector("#data-struktur").innerHTML += `
+    <div class="text-xl text-white text-center">Angkatan ${element}</div>
+    <div class="grid grid-rows-1 gap-4 h-[50vh] overflow-x-scroll grid-flow-col">
+  ${curHtml}
+  </div>
+  `;
+
+  }
+	// document.querySelector("#data-struktur").innerHTML += `
+  // <div class="grid grid-rows-2 lg:grid-rows-3 gap-4 h-screen overflow-x-scroll grid-flow-col">
+  // ${html}
+  // </div>
+  // `;
+	const swiper = new Swiper("#data-struktur", {
+		horizontal: true,
+	});
 }
 
-main()
+main();
